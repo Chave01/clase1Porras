36cd6074d4b6ab50d313ee5ff18b5aa282827dd4
Chavella Porras <chavellaporras@Chavellas-MacBook-Pro.local>
2021-07-30 16:16:39 -0300

    mi primer algoritmo

---
+++ app.js
@@ -0,0 +1,11 @@
+let Nombreingresado = prompt ("Ingresar Nombre")
+let Apellidoingresado = prompt ("Ingresar Apellido")
+let Contraseña = prompt ("Ingresar Contraseña")
+
+
+if ((Nombreingresado == "chavella") && (Contraseña == "portal123")){
+        alert ("Podes entrar")
+
+} else {
+    alert ("No podes entrar")
+}
